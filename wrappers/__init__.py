a23d2105e0860292a75f327375c3e5101a5c402d
戚瀚予 <3619754257@qq.com>
2026-08-14 20:40:08 +0800

    Revert "Rebuild native evaluation pipeline"
    This reverts commit a3dbf20dbb135ced9ef942da732764e1a2a116ab.

--- wrappers/__init__.py
+++ wrappers/__init__.py
@@ -1,9 +1,2 @@
 from .intention_wrapper import IntentionRewardWrapper
 from .dense_wrapper import DenseDistanceWrapper
-from .direction_observation import DirectionalImageObservation
-from .native_metrics import NativeRewardInfoWrapper
-from .potential_wrappers import (
-    GeometricPotentialWrapper,
-    OptimalPathPotentialWrapper,
-    WrongDirectionPotentialWrapper,
-)
